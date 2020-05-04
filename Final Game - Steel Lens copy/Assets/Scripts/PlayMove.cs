de8dac1d5807630993abd8b8cf51b258eb7933c7
Raen-Sz <60935009+Raen-Sz@users.noreply.github.com>
2020-05-04 11:21:39 -0400

    Steel lens Progress?
    I think this is everything I have so far on Steel Lens, My final Game Project.

---
+++ Final Game - Steel Lens copy/Assets/Scripts/PlayMove.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayMove : MonoBehaviour
+{
+    public float speed;
+    Vector3 pos;
+    Vector3 prePos;
+
+    public float StealthLength;
+
+    bool Cloak = false;
+
+    SpriteRenderer sr;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        pos = transform.position;
+        sr = GetComponent<SpriteRenderer>();
+
+    }
+
+
+    void Update()
+    {
+        if (transform.position == pos)
+        {
+            if (Input.GetButtonDown("up")) pos.y += 4;
+
+            if (Input.GetButtonDown("down")) pos.y -= 4;
+
+            if (Input.GetButtonDown("left")) pos.x -= 4;
+
+            if (Input.GetButtonDown("right")) pos.x += 4;
+
+            prePos = pos;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pos, speed * Time.deltaTime);
+
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetButtonDown("sneak"))
+        {
+            StartCoroutine(Stealth());
+        }
+    }
+
+    IEnumerator Stealth()
+    {
+        Cloak = true;
+        sr.color = Color.blue;
+        yield return new WaitForSeconds(StealthLength);
+        sr.color = Color.white;
+        Cloak = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Beam") && Cloak == false)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            pos = prePos;
+        }
+    }
+
+}
